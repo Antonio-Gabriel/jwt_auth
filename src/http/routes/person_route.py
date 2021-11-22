d5fd04bbb86120a45312c5485e2d2afc1259f254
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-11-22 21:51:42 +0100

    create person route

--- src/http/routes/person_route.py
+++ src/http/routes/person_route.py
@@ -4,25 +4,27 @@ from src.repositories import PersonRepository
 
 person_route_bp = Blueprint('api_routes', __name__)
 
+
 @person_route_bp.route('/', methods=['GET'])
-def index():    
+def index():
     """Preview persons
 
     Returns:
         [json]: [persons]
     """
     return jsonify({"data": "oi"})
-    #return jsonify({"status_code": 200, "data": "response"})
+    # return jsonify({"status_code": 200, "data": "response"})
 
-@person_route_bp.route('/person/create', methods=['GET'])
-def create_person():    
-    
+
+@person_route_bp.route('/person/create', methods=['POST'])
+def create_person():
+
     person_repository = PersonRepository(request_api=request)
     response = person_repository.create_person()
-    
+
     if response == None or response == {}:
         return jsonify({"status_code": 409, "msg": {
             "error": "Error"
-        }})    
+        }})
 
     return jsonify({"status_code": 200, "data": [response]})
