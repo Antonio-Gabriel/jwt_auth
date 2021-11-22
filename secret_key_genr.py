d39e3b9459c5c99060fa8f0bdc0cb904664254c0
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-11-22 18:17:22 +0100

    random secret key config

--- secret_key_genr.py
+++ secret_key_genr.py
@@ -2,11 +2,14 @@
 
 import os
 from uuid import uuid4
+from secrets import token_urlsafe
 
 urandom_delimiter = os.urandom(12)
-secret_key_generate_oub = uuid4.uuid4().hex
+secret_key_generate_oub = uuid4().hex
+urlsafe_token = token_urlsafe(12)
 
 print(
     urandom_delimiter,
-    secret_key_generate_oub
+    secret_key_generate_oub,
+    urlsafe_token
 )
